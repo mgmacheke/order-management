2916115d5f5b7dfc15bde84475ce89aa4f0f475f
MusaMacheke <musa@bancx.com>
2026-03-28 08:04:07 +0200

    add add orderItem request and respond dtos.

---
+++ src/main/java/com/pollinate/order/management/dto/OrderItemResponse.java
@@ -0,0 +1,31 @@
+package com.pollinate.order.management.dto;
+
+public class OrderItemResponse {
+    private Long productId;
+    private String productName;
+    private int quantity;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
