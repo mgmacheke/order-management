49a06202f366b4b7fa0015923d17a3f0678ae6f4
MusaMacheke <musa@bancx.com>
2026-03-28 07:42:32 +0200

    add product

---
+++ src/main/java/com/pollinate/order/management/model/Product.java
@@ -0,0 +1,52 @@
+package com.pollinate.order.management.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
